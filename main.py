bf0f994c4c0f8ad748eade5f02409dbcb7211512
ForceCore <ssanzing@gmail.com>
2019-03-01 15:10:54 +0900

    ADD limit and market order handler

--- main.py
+++ main.py
@@ -50,14 +50,93 @@ class PriceHandler(RequestHandler):
         self.write(odata)
 
 
+class MarketOrderHandler(RequestHandler):
+    def post(self):
+        data = tornado.escape.json_decode(self.request.body)
+        print("MarketOrderHandler: incoming")
+        print(data)
+
+        assert data['OrdQty'] != 0
+        bnstpcode = 2 if data['OrdQty'] < 0 else 1 # 2 for sell, 1 for buy.
+        data['OrdQty'] = abs(data['OrdQty'])
+
+        q = xing.xaquery.Query("CSPAT00600")
+        result = q.request(
+            {
+                "InBlock1": {
+                    "AcntNo": data["AcntNo"],
+                    "InptPwd": config.account["pass"],
+                    "IsuNo": data["IsuNo"],
+                    "OrdQty": data["OrdQty"],
+                    "OrdPrc": 0, # 0 for market order
+                    "BnsTpCode": bnstpcode, # "1" for sell, "2" for buy
+                    "OrdprcPtnCode": "03", # "00": limit order, "03" market, "61" jang jeon shi gan oe
+                    "MgntrnCode": "000", # margin trading code
+                    "LoanDt": "", # Dae chool date
+                    "OrdCndiTpCode": "0" # order condition code, Nil, TOC, FOK stuff.
+                }
+            },
+            {
+                "OutBlock1": ("RecCnt",),
+                "OutBlock2": ("RecCnt",)
+            }
+        )
+
+        print("Response to client:")
+        print(result)
+        self.write(result)
+
+
+class LimitOrderHandler(RequestHandler):
+    def post(self):
+        data = tornado.escape.json_decode(self.request.body)
+        print("LimitOrderHandler: incoming")
+        print(data)
+
+        assert data['OrdQty'] != 0
+        bnstpcode = "2" if data['OrdQty'] < 0 else "1" # 2 for sell, 1 for buy.
+        if data['OrdQty'] < 0:
+            data['OrdQty'] *= -1
+
+        q = xing.xaquery.Query("CSPAT00600")
+        result = q.request(
+            {
+                "InBlock1": {
+                    "AcntNo": data["AcntNo"],
+                    "InptPwd": config.account["pass"],
+                    "IsuNo": data["IsuNo"],
+                    "OrdQty": data["OrdQty"],
+                    "OrdPrc": data["OrdPrc"], # 0 for market order
+                    "BnsTpCode": bnstpcode, # "1" for sell, "2" for buy
+                    "OrdprcPtnCode": "00", # "00": limit order, "03" market, "61" jang jeon shi gan oe
+                    "MgntrnCode": "000", # margin trading code
+                    "LoanDt": "", # Dae chool date
+                    "OrdCndiTpCode": "0" # order condition code, Nil, TOC, FOK stuff.
+                }
+            },
+            {
+                "OutBlock1": ("RecCnt",),
+                "OutBlock2": ("RecCnt",)
+            }
+        )
+
+        print("Response to client:")
+        print(result)
+        self.write(result)
+
+
 def make_app():
     urls = [
-        ("/price", PriceHandler)
+        ("/price", PriceHandler),
+        ("/market", MarketOrderHandler),
+        ("/limit", LimitOrderHandler)
     ]
     return Application(urls, debug=True)
 
 
-session = xing.xasession.Session()
+def shutdown():
+    print("Logging out")
+    session.logout()
 
 
 if __name__ == "__main__":
@@ -73,10 +152,10 @@ if __name__ == "__main__":
 
     app = make_app()
     app.listen(5000)
+    tornado.autoreload.add_reload_hook(shutdown)
     print("Starting server")
 
     try:
         IOLoop.instance().start()
     except KeyboardInterrupt:
-        print("Logging out")
-        session.logout()
+        shutdown()
--- sample_config.py
+++ sample_config.py
@@ -1,14 +1,13 @@
 server = {
-    "address": "127.0.0.1",
+    "address": "127.0.0.1", # For xingACE
+    #"address": "hts.etrade.co.kr", # For real trading
     "port": 20001,
-    "type": 0
+    "type": 0 # ?? Fixed as 0
 }
+
 user = {
     "id": "your_id",
     "passwd" : "your_pass",
+    "account_passwd": "1234",
     "certificate_passwd" : "your_cert_pass"
-}
-accout = {
-    "number": "account",
-    "pass": "pass"
 }
